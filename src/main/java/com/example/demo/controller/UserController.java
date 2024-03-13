e16b1aa4305b1d4dbebc6b51661b343fae8cd286
Sebastian Gruenewald <wi21b080@technikum-wien.at>
2024-03-13 10:46:32 +0100

    vulnerability to check codeql

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,25 +2,28 @@ package com.example.demo.controller;
 
 import com.example.demo.model.UserSpring;
 import com.example.demo.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @PostMapping("/register")
-    public String registerUser(@RequestBody UserSpring user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return "User registered successfully!";
+    @GetMapping("/users/{username}")
+    public List<UserSpring> getUserByUsername(@PathVariable String username) {
+        Query query = entityManager.createNativeQuery("SELECT * FROM users WHERE username = '" + username + "'", UserSpring.class);
+        return query.getResultList();
     }
 }
